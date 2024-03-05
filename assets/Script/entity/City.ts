c94943cea3c7a98e914659b768403c7b2b17c7c1
Shashank <shashank.chaurasia@gamifystudios.co>
2024-03-05 07:12:59 +0530

    added tile and color to city

--- assets/Script/entity/City.ts
+++ assets/Script/entity/City.ts
@@ -7,10 +7,25 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class City extends Property {
 
+    @property(cc.Node)
+    tileNode: cc.Node = null;
+
+    @property(cc.Node)
+    tileColorNode: cc.Node = null;
+
     onLoad() {
         super.onLoad();
         this.pNameLabel.string = this.data.name;
         this.pPriceLabel.string = String(this.data.price);
+    }
+
+    setSide(side: number) {
+        super.setSide(side);
+        if (side == 1 || side == 2) {
+            // Top and Right
+            // this.userIcon.angle = 90;
+            this.tileColorNode.setPosition(0, 75);
+        }
     }
 
     onClick() {
